6176c8f6dc6f31e29d3c1876682332e8e828f674
Hope <hopegiometti@gmail.com>
2019-10-12 15:09:49 -0400

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,46 @@
 class Waiter
+    attr_accessor :name, :yrs_experience
+
+    @@all = []
+
+    def initialize(name, yrs_experience)
+        @name = name
+        @yrs_experience = yrs_experience
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.waiter == self
+        end
+        #returns an array of all of the meals served by this waiter
+    end
+
+    def best_tipper
+        #return the customer instance associated w/ that meal
+        biggest_tip = 0
+        tipper = nil
+
+        meals.each do |meal|
+                
+                if meal.tip > biggest_tip
+                    biggest_tip = meal.tip
+                    tipper = meal.customer
+                    # binding.pry
+                end
+        end
+        tipper
+        # binding.pry
+        # tipper
+        
+    end
 
 end
